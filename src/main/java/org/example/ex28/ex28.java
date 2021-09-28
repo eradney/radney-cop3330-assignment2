b53775306a8a5cc75a3fff04e03070a12bd990f0
eradney <eradney@knights.ucf.edu>
2021-09-27 22:18:17 -0400

    ex28

--- src/main/java/org/example/ex28/ex28.java
+++ src/main/java/org/example/ex28/ex28.java
@@ -3,11 +3,24 @@
  *  Copyright 2021 Emily Radney
  */
 
-package org.example.ex24;
+package org.example.ex28;
 import java.util.Scanner;
 
 public class ex28 {
     public static void main( String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int [] ph = new int[5];
+        int sum = 0;
 
+        for(int i = 0; i < 5; i++) {
+            System.out.print("Enter a number: ");
+            ph[i] = scnr.nextInt();
+        }
+
+        for (int j = 0; j < ph.length; j++) {
+            sum += ph[j];
+        }
+
+        System.out.println("The total is " + sum + ".");
     }
 }
---
+++ src/main/java/org/example/ex29/ex29.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Emily Radney
+ */
+
+package org.example.ex29;
+import java.util.Scanner;
+
+
+public class ex29 {
+    public static void main( String[] args) {
+
+    }
+}
