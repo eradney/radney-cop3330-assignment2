58f34ca2e0e313e885646a9c8edabb0d41cb6c1b
eradney <eradney@knights.ucf.edu>
2021-09-27 22:28:02 -0400

    ex29

--- src/main/java/org/example/ex29/ex29.java
+++ src/main/java/org/example/ex29/ex29.java
@@ -9,6 +9,25 @@ import java.util.Scanner;
 
 public class ex29 {
     public static void main( String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double rate, years;
 
+        System.out.print("What is the rate of return? ");
+
+        while (true) {
+            if (scnr.hasNextDouble()) {
+                rate = scnr.nextDouble();
+
+                if (rate > 0.0) {
+                    years = Math.ceil(72 / rate);
+                    System.out.println("It will take " + String.format("%.0f",years) + " years to double your initial investment.");
+                    break;
+                }
+            }
+
+            System.out.println("Sorry, that is not a valid input.");
+            scnr.nextLine();
+            System.out.print("What is the rate of return? ");
+        }
     }
 }
